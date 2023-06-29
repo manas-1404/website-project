d718595cc3276fa9ee77916c4fb241ff57250e85
dark_king <manas.gowda04@yahoo.com>
2023-06-30 02:17:01 +0530

    Added about-me.js and work-experience.js Modified App.js Commented out App.css and index.css

---
+++ src/about-me.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const content = "I'm a passionate computer science student. I am highly interested in the area of Machine Learning and Natural Language Processing"
+
+function AboutMe(){
+    return (
+
+        <div>
+            <h2><strong>About Me</strong></h2>
+            <p>{content}</p>
+        </div>
+        
+    )
+}
+
+export default AboutMe
---
+++ src/work-experience.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function WorkExperience(){
+    return (
+        <div id="work-experience">
+            
+        </div>
+    )
+}
