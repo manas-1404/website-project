1aa571101c2504f07008a69fc280d95a6e6482b4
dark_king <manas.gowda04@yahoo.com>
2023-07-04 21:15:15 +0530

    First Page is complete along with Styling Completed Styling of NavBar and First Page containing image and name

--- src/first-page.js
+++ src/first-page.js
@@ -2,18 +2,19 @@ import React from "react";
 
 import Manas from "./images/manas.jpg";
 
-function FirstPage(){
-    return(
-        <div>
-            <div id="MyImage">
-                <img src={Manas} alt="Professional Picture of Manas" width="30%"/>
-            </div>
-            <div id="IamManas">
-                <h1>Hello!</h1>
-                <h1>I'm</h1>
-                <h1><strong>Manas!</strong></h1>
+function FirstPage() {
+    return (
+        <div className="First-Page-stage">
+            <div id="First-Page">
+                <img src={Manas} alt="Professional Picture of Manas" width="37%" id="MyImage" />
+                <div id="IamManas">
+                    <h1>Hello!</h1>
+                    <h1>I'm</h1>
+                    <h1><strong>Manas!</strong></h1>
+                </div>
             </div>
         </div>
+
     );
 }
 
--- src/nav-bar.js
+++ src/nav-bar.js
@@ -7,19 +7,17 @@ function NavBar() {
         <nav className="Nav-Bar">
 
             <div className="Website-Logo">
-                <img src={Logo} alt="Manas Logo" width="10%" id="Logo"/>
+                <img src={Logo} alt="Manas Logo" width="10%" id="Logo" />
                 <div>
-                    <button id="MyName"><a href="#Logo">Manas Srinivas Gowda</a></button>
+                    <p id="MyName"><a href="#Logo">Manas Gowda</a></p>
                 </div>
             </div>
 
-            {/* <img src={Logo} alt="Manas Logo" width="5%" id="Logo"/>
-            <button><a href="#Logo">Manas Srinivas Gowda</a></button> */}
-            <button><a href="#Work-Experience">Work Experience</a></button>
-            <button><a href="#Education">Education</a></button>
-            <button><a href="#Projects">Project</a></button>
-            <button><a>Resume</a></button>
-            <button><a href="#Contact-Me">Contact Me</a></button>
+            <a href="#Work-Experience">Work Experience</a>
+            <a href="#Education">Education</a>
+            <a href="#Projects">Project</a>
+            <a>Resume</a>
+            <a href="#Contact-Me">Contact Me</a>
 
         </nav>
     );
