a21c4ba6062d42a3ab15bde4d047b1baa2df9af4
dark_king <manas.gowda04@yahoo.com>
2024-03-08 10:30:51 -0700

    Resolved Merge Requests and Added Router & Routes

--- src/App.js
+++ src/App.js
@@ -9,32 +9,44 @@ import './styling/contact-me.css';
 
 import './styling/dark-mode.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 // importing other components from different files
 import NavBar from './nav-bar';
 import FirstPage from "./first-page";
-import AboutMe from "./about-me"; 
+import AboutMe from "./about-me";
 import WorkExperience from './work-experience';
 import Education from "./education"
 import Projects from "./project";
 import ContactMe from './contact-me';
+import { DarkModeProvider } from './dark-mode';
 
 import "./images/placeholder.png";
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <NavBar />
-        <FirstPage />
-        <AboutMe />
-        <WorkExperience />
-        <Education />
-        <Projects />
-        <ContactMe />
-      </header>
-    </div>
+    <DarkModeProvider>
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+          <header className="App-header">
+            <Routes>
+              <Route path="/" element={<FirstPage />} />
+              <Route path="/about-me" element={<AboutMe />} />
+              <Route path="/experience" element={<WorkExperience />} />
+              <Route path="/education" element={<Education />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact-me" element={<ContactMe />} />
+              <Route path="*" element={<FirstPage />} />
+            </Routes>
+          </header>
+          <footer>
+            <p>&copy; 2023 Manas Srinivas Gowda. All rights reserved.</p>
+          </footer>
+        </div>
+      </BrowserRouter>
+    </DarkModeProvider>
   );
 }
 
--- src/first-page.js
+++ src/first-page.js
@@ -1,20 +1,24 @@
 import React from "react";
 
+import AboutMe from "./about-me";
+
 import Manas from "./images/manas.jpg";
 
 function FirstPage() {
     return (
-        <div id="First-Page-stage">
-            <div id="First-Page">
-                <img src={Manas} alt="Professional Picture of Manas" width="37%" id="MyImage" />
-                <div id="IamManas">
-                    <h1>Hey!</h1>
-                    <h1>This is</h1>
-                    <h1><strong>Manas!</strong></h1>
+        <>
+            <div id="First-Page-stage">
+                <div id="First-Page">
+                    <img src={Manas} alt="Manas" width="37%" id="MyImage" />
+                    <div id="IamManas">
+                        <h1>Hey!</h1>
+                        <h1>This is</h1>
+                        <h1><strong>Manas!</strong></h1>
+                    </div>
                 </div>
             </div>
-        </div>
-
+            <AboutMe />
+        </>
     );
 }
 
