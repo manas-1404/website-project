90c505e154675f0959e7c1ac8367a3164389b464
dark_king <manas.gowda04@yahoo.com>
2024-03-10 16:58:16 -0700

    Fixed Security Risk in Contact Me Tab

--- src/contact-me.js
+++ src/contact-me.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //importing pictures from images
-import PlaceHolder from "./images/placeholder.png";
+// import PlaceHolder from "./images/placeholder.png";
 import LinkedIn from "./images/linkedin.png";
 import GitHub from "./images/github.png";
 import Resume from "./images/resume.png";
@@ -23,9 +23,9 @@ function ContactMe() {
                 {/* make the below div as a flexbox */}
                 <div id="image-container">
 
-                    <a href="https://github.com/manas-1404" target="_blank"><Picture img={GitHub} alt="GitHub" /></a>
-                    <a href="https://www.linkedin.com/in/manas-gowda/" target="_blank"><Picture img={LinkedIn} alt="LinkedIn" /></a>
-                    <a href="https://drive.google.com/file/d/1lBZEPW5pCB75ekDeMvzazDI5wLTtUUiJ/view?usp=sharing" target="_blank">
+                    <a href="https://github.com/manas-1404" target="_blank" rel="noreferrer noopener"><Picture img={GitHub} alt="GitHub" /></a>
+                    <a href="https://www.linkedin.com/in/manas-gowda/" target="_blank" rel="noreferrer noopener"><Picture img={LinkedIn} alt="LinkedIn" /></a>
+                    <a href="https://drive.google.com/file/d/1lBZEPW5pCB75ekDeMvzazDI5wLTtUUiJ/view?usp=sharing" target="_blank" rel="noreferrer noopener">
                         <Picture img={Resume} alt="Resume" />
                     </a>
                 </div>
